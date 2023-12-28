3b1a4ef8ea909f21667014a730fbdc385c09315a
BranchManager <nbranch@vols.utk.edu>
2023-12-27 23:57:39 -0500

    set up wacom tablet on hyprland and installed unity

--- .config/eww/scripts/start_eww.sh
+++ .config/eww/scripts/start_eww.sh
@@ -73,14 +73,20 @@ then
         do
             if [[ "${focused_array[$i]}" == "yes" ]]
             then
-            
+                #the above lines from the "focussed_array to 'len=' grabe the monitor that has the output listed below as an example"
+                #Monitor DP-2 (ID 1):
+	            #focused: yes
+
                 eww_monitor="main_"
                 monitor_num="${focused_array[$i-2]:0:1}"
                 eww_monitor+=$((monitor_num))
                 eww update anim_open_ostg=true & #this is for the slide out animation
+
                 #echo "THE MONITOR WE ARE OPENING and number"
                 #echo $eww_monitor
                 # echo $monitor_num
+
+                #once we grab the ID we make the string "main_1 (for example) ie main_# and our eww code will open the window on that monitor"
                 eww open $eww_monitor &
                 python3 ~/.config/eww/scripts/notion_test.py &
                 python3 ~/.config/eww/scripts/weather.py & 
--- .config/waybar/rofi_configs/idlelock.sh
+++ .config/waybar/rofi_configs/idlelock.sh
@@ -1,8 +1,8 @@
 swayidle -w  \
 	timeout 600 'hyprctl dispatch dpms off' \
-	resume 'hyprctl dispatch dpms on' \
-	timeout 900 '~/.config/waybar/swaylock_min.sh' \
-	resume 'hyprctl dispatch dpms on'\
+	resume 'hyprctl dispatch dpms on' 
+	#timeout 900 '~/.config/waybar/swaylock_min.sh' \
+	#resume 'hyprctl dispatch dpms on'\
 	
 	#
 	#timeout 10 '/home/branchmanager/.config/waybar/swaylock_after_id
